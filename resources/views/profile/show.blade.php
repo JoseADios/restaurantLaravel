73f0bffe3858473d42f481758d2e3b89e846abef
JoseA <josedios10@gmail.com>
2023-03-27 17:00:06 -0400

    Menu innecesario superior jetstream

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -1,7 +1,13 @@
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+@section('content')
+
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Profile') }}
+            {{ __('Perfil de usuario') }}
         </h2>
     </x-slot>
 
@@ -43,3 +49,5 @@
         </div>
     </div>
 </x-app-layout>
+    
+@endsection
