fdd2fa6f3ffad690e82a20815271cd046f367567
JoseA <josedios10@gmail.com>
2023-04-17 22:28:04 -0400

    Meta accept mixed cont

--- resources/views/vendor/adminlte/page.blade.php
+++ resources/views/vendor/adminlte/page.blade.php
@@ -13,6 +13,7 @@
 @section('body_data', $layoutHelper->makeBodyData())
 
 @section('body')
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <div class="wrapper">
 
         {{-- Preloader Animation --}}
